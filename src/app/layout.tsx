cd72d42df37bda51bf988c9bda72b7fb1a55b899
diego crispim <diego.crispim@yahoo.com.br>
2026-05-10 09:19:51 -0300

    Refactor global styles and layout: enhance CSS variables for theming, integrate dark mode support, and implement loading state in the main page. Add a NotFound component for redirection to the homepage.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,10 +25,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`dark ${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
     </html>
   );
 }
+
